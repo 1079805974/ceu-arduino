5adaf230b2b6b4879158cd806efe95cca1bcab76
ljr <1079805974@qq.com>
2019-03-14 23:29:15 +0800

    update examples

--- c_examples/button-02.ino
+++ c_examples/button-02.ino
@@ -1,20 +1,14 @@
-bool started = false;
 
 void setup() {
   pinMode(A0, INPUT);
   pinMode(2, INPUT);
   Serial.begin(9600);
-  attachInterrupt(0, changeStatus, CHANGE);
+  attachInterrupt(0, changeStatus, readADC);
 }
 
-void loop() {
-  delay(1000);
-  if(started){
-    int i = analogRead(A0);
-    Serial.println(i);
-  }
+void readADC(){
+  int i = analogRead(A0);
+  Serial.println(i);
 }
 
-void changeStatus(){
-  started = !started;
-}
+void loop(){}
