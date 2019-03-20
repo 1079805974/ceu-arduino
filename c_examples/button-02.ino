ff037ac209f97440112f1661708169530caa884f
1079805974 <1079805974@qq.com>
2019-03-21 00:43:10 +0800

    temp save

--- c_examples/button-02.ino
+++ c_examples/button-02.ino
@@ -1,14 +1,29 @@
+#define LED 3
+#define NIGHT 80
+
+bool started = false;
 
 void setup() {
-  pinMode(A0, INPUT);
-  pinMode(2, INPUT);
-  Serial.begin(9600);
-  attachInterrupt(0, changeStatus, readADC);
+  sei();
+  adc_init();
+  pinMode(LED,OUTPUT);
 }
 
-void readADC(){
-  int i = analogRead(A0);
-  Serial.println(i);
+void loop() {
+  
 }
 
-void loop(){}
+void adc_init(void)
+{
+  ADMUX |= 1<<REFS0 | 1<<MUX0 ;
+  ADCSRA |= (1<<ADEN)  |(1<<ADSC)| (1<<ADATE) |(1<<ADIE);
+}
+
+ISR(ADC_vect) {
+  if(ADC > NIGHT){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+
+}
